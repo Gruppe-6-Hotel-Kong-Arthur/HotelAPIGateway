95342388e99e5f9629b581cc8e39c3b5e385b256
CBT MacBook <Christianbt96@gmail.com>
2024-10-31 10:30:54 +0100

    it works

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # HotelAPIGateway
+
+
+docker build -t api_gateway . && docker image prune -f
+
+docker rm -f api_gateway && docker run -d -p 5010:5010 -e GUEST_SERVICE_URL=http://guest_service:5001 -e ROOM_INVENTORY_SERVICE_URL=http://room_inventory_service:5002 -e RESERVATION_SERVICE_URL=http://reservation_service:5003 -e DRINKS_SERVICE_URL=http://drinks_service:5004 -e CSV_EXPORT_SERVICE_URL=http://csv_export_service:5005 -e DRINKS_SALES_SERVICE_URL=http://drinks_sales_service:5006 --name api_gateway --network microservice-network api_gateway
--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
 MICROSERVICES = {
-    "room_inventory_service": "http://localhost:5002",
-    "reservation_service": "http://localhost:5003",
-    "CSV_export_service": "http://localhost:5005",
-    "drinks_service": "http://localhost:5004",
-    "drinks_sales_service": "http://localhost:5006",
-    "guest_service": "http://localhost:5001"
+    "room_inventory_service": os.getenv("ROOM_INVENTORY_SERVICE_URL", "http://localhost:5002"),
+    "reservation_service": os.getenv("RESERVATION_SERVICE_URL", "http://localhost:5003"),
+    "CSV_export_service": os.getenv("CSV_EXPORT_SERVICE_URL", "http://localhost:5005"),
+    "drinks_service": os.getenv("DRINKS_SERVICE_URL", "http://localhost:5004"),
+    "drinks_sales_service": os.getenv("DRINKS_SALES_SERVICE_URL", "http://localhost:5006"),
+    "guest_service": os.getenv("GUEST_SERVICE_URL", "http://localhost:5001"),
 }
 
 @app.route('/<service>/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
@@ -34,4 +35,4 @@ def gateway(service, path):
     return (response.content, response.status_code, response.headers.items())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5010)
